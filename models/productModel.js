afa3085a98a05e1ccb51d1872969ff1d384057d2
nairx <nairxprv@gmail.com>
2026-03-09 09:40:19 +0530

    updated

--- models/productModel.js
+++ models/productModel.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const productSchema = mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
+  imageUrl: {type:String}
 });
 const  productModel = mongoose.model("products",productSchema)
 
